7a61ec5bfb7c782f01b829baa238c89458c5824e
SobhanGhadimi <sobhamghwork@gmail.com>
2024-10-29 06:44:18 -0700

    fix/api and style

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 const apiUrl = "https://api.adviceslip.com/advice";
+
+function fetchData(e) {
+  e.preventDefault();
+
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("an error occurred");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const slip = data.slip;
+      const id = slip.id;
+      const advice = slip.advice;
+
+      console.log(`id = ${id} and advice is: "${advice}"`);
+    })
+    .catch((error) => {
+      console.error("there was an error here:", error);
+    });
+}
+
+document.addEventListener("click", fetchData);
