2d120193fad8f238ad75323e66f462611506a671
SobhanGhadimi <sobhamghwork@gmail.com>
2024-10-29 04:03:07 -0700

    fix/ design

--- js/main.js
+++ js/main.js
@@ -1,14 +1,1 @@
 const apiUrl = "https://api.adviceslip.com/advice";
-
-async function fetchData() {
-
-try{
-const response = await fetch(apiUrl);
-
-if(!response.ok){
-throw new Error("network is facing isues" + response.statusText)
-}
-const data = await response.json();
-}
-
-}
